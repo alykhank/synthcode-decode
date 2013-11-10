ad3ecae331fa97500c8441b29a5be1df0101c1fc
Alykhan Kanji <github@alykhan.com>
2013-11-10 18:54:44 -0500

    Attempt base conversion

--- analyze.py
+++ analyze.py
@@ -4,6 +4,10 @@ import os, json
 from pyechonest import track
 import requests
 from decode import detectKeys
+from baseconv import BaseConverter
+
+base27 = BaseConverter('abcdefghijklmnopqrstuvwxyz ')
+base13 = BaseConverter('0123456789abc')
 
 if os.environ.get('ID'):
 	t = track.track_from_id(os.environ.get('ID'))
@@ -15,4 +19,22 @@ r = requests.get(t.analysis_url).content
 segments = json.loads(r)['segments']
 print json.dumps(segments, indent=4)
 
-print detectKeys(segments)
+keys = detectKeys(segments)
+
+for idx, k in enumerate(keys):
+	if k is 10:
+		keys[idx] = 'a'
+	elif k is 11:
+		keys[idx] = 'b'
+	elif k == 12:
+		keys[idx] = 'c'
+	else:
+		keys[idx] = str(k)
+
+print 'Keys:', keys
+
+keyString = ''.join(keys)
+keys_10 = base13.decode(keyString)
+print 'Base 10:', keys_10
+keys_27 = base27.encode(keys_10)
+print 'Base 27:', keys_27
