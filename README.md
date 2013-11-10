ee90ff7cc2f12ca7b0aea50c16e8c5a8d06fd056
Alykhan Kanji <github@alykhan.com>
2013-11-10 12:17:40 -0500

    Add Usage section to README.md

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # synthcode-decode
 
 Uses The Echo Nest to decode synthcodes.
+
+## Usage
+
+Ensure $ECHO_NEST_API_KEY is set.
+Set $FILE to audio filename for new files, once tracks been uploaded set $ID for repeat analysis.
--- analyze.py
+++ analyze.py
@@ -8,6 +8,7 @@ if os.environ.get('ID'):
 	t = track.track_from_id(os.environ.get('ID'))
 else:
 	t = track.track_from_filename(os.environ.get('FILE'))
+	print 'ID:', t.id
 r = requests.get(t.analysis_url).content
 rj = json.loads(r)['segments']
 print json.dumps(rj, indent=4)
